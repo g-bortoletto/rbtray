fa64904e0f10d46110f07d8c44d006254ebb8a17
Guilherme Bortoletto <guilherme.mbortoletto@gmail.com>
2024-01-10 08:14:26 -0300

    Remove CTRL+ALT+DOWN shortcut

--- RBTray.cpp
+++ RBTray.cpp
@@ -376,19 +376,10 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE /*hPrevInstance*
 
     WM_TASKBAR_CREATED = RegisterWindowMessage(L"TaskbarCreated");
 
-    BOOL registeredHotKey = RegisterHotKey(_hwndHook, 0, MOD_ALT | MOD_CONTROL, VK_DOWN);
-    if (!registeredHotKey) {
-        MessageBox(NULL, L"Couldn't register hotkey", L"RBTray", MB_OK | MB_ICONERROR);
-    }
-
     MSG msg;
     while (GetMessage(&msg, nullptr, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
-    }
-
-    if (registeredHotKey) {
-        UnregisterHotKey(_hwndHook, 0);
     }
 
     return (int)msg.wParam;
